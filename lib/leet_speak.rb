0341339751a5d85f453edf0cdb5d4e7dc6a7cefd
Brian Grant and Molly McGlone <brian@>
2016-06-27 15:12:15 -0700

    add spec for handling sentence

--- lib/leet_speak.rb
+++ lib/leet_speak.rb
@@ -1,18 +1,30 @@
 class String
   define_method (:leet_speak) do
-    letters = self.split("")
-    new_letters = []
-    letters.each do |letter|
-      if letter === "e"
-        new_letters.push("3")
-      elsif letter === "o"
-        new_letters.push("0")
-      elsif letter === "I"
-        new_letters.push("1")
-      else
-        new_letters.push(letter)
+    sentence_split = self.split
+    final_sentence = []
+    sentence_split.each do |word|
+      letters = word.split("")
+      new_letters = []
+      if letters.at(0) === "s"
+        new_letters.push("s")
+        letters.shift()
       end
+      letters.each do |letter|
+        if letter === "e"
+          new_letters.push("3")
+        elsif letter === "o"
+          new_letters.push("0")
+        elsif letter === "I"
+          new_letters.push("1")
+        elsif letter === "s"
+          new_letters.push("z")
+        else
+          new_letters.push(letter)
+        end
+      end
+      new_word = new_letters.join("")
+      final_sentence.push(new_word)
     end
-    new_letters.join("")
+  final_sentence.join(" ")
   end
 end
--- spec/leet_speak_spec.rb
+++ spec/leet_speak_spec.rb
@@ -14,13 +14,13 @@ describe('String#leet_speak') do
  it('replaces every "I" in a string with a "1"') do
     expect("I like Ike".leet_speak).to(eq("1 lik3 1k3"))
   end
- # it('replaces every "s" in a string with a "z"') do
- #    expect("roses".leetspeak).to(eq("r0z3z"))
- #  end
- #  it('does NOT replace the first letter when it is an s') do
- #    expect("sassafrass".leetspeak).to(eq("sazzafrazz"))
- #  end
- #  it('replaces letters correctly in a string of words') do
- #    expect("I scream you scream we all scream for raspberry ice cream.".leetspeak)).to(eq("1 scr3am y0u scr3am w3 all scr3am f0r razpb3rry ic3 cr3am"))
- #  end
+ it('replaces every "s" in a string with a "z"') do
+    expect("roses".leet_speak).to(eq("r0z3z"))
+  end
+  it('does NOT replace the first letter when it is an s') do
+    expect("sassafrass".leet_speak).to(eq("sazzafrazz"))
+  end
+  it('replaces letters correctly in a string of words') do
+    expect("I scream you scream".leet_speak).to(eq("1 scr3am y0u scr3am"))
+  end
 end
